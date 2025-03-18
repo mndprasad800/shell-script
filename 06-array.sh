daab2ed405787312035fe8327ecd998bafdec3e0
Prasad7901 <mndprasad1999@gmail.com>
2025-03-18 13:46:38 +0530

    arrays topic in shell script

--- 06-array.sh
+++ 06-array.sh
@@ -4,11 +4,11 @@ movies=("RRR" "BAHUBALI" "MURARI")
 
 FRUITS=("MANGO" "APPLE" "BANANA")
 
-echo "first movie is $movies[0]"
+echo "first movie is ${movies[0]}"   #if we are not use the flower braces the o/p is RRR[0]
 
 echo "first fruit is ${FRUITS[0]}"
 
-echo "list all the movies: $movies[@]"  # @ denotes entire array
+echo "list all the movies: ${movies[@]}"  # @ denotes entire array
 
-echo "list all the fruits: ${FRUITS[@]}"    # we can use variables with or without flower braces
+echo "list all the fruits: ${FRUITS[@]}"    # so,we must use flower braces in arrays
 
