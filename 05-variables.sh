726302ecd62ac9b08ccc6652243ea3ae293c7d67
Prasad7901 <mndprasad1999@gmail.com>
2025-03-18 13:25:22 +0530

    variables program in shell script

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "please enter your username::"
+
+read USERNAME
+
+echo "please enter yout password::"
+
+read PASSWORD
+
+echo "your username is: $USERNAME, your password is: $PASSWORD"
